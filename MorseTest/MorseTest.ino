3343ca64ba7294bd29e590318e8b20b2aeea3d0e
Ryan Zhao <ryanzhao@live.com>
2018-05-31 10:02:31 -0400

    morse code encoder

--- MorseTest/MorseTest.ino
+++ MorseTest/MorseTest.ino
@@ -1,5 +1,4 @@
 #include <LiquidCrystal.h>
-#include "pitches.h"
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
@@ -7,36 +6,45 @@ const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 const int buttonPin = 7;
-int numClicks = 0;
 bool previousState = false;
+int timeSinceLastChange = 0;
+String code = "";
 
 void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("Time:");
-  lcd.setCursor(0,1);
-  lcd.print("NumClicks:");
+
+  timeSinceLastChange = millis();
 
   pinMode(buttonPin, INPUT);
 }
 
 void loop() {
   // set the cursor to column 0, line 1
-  lcd.setCursor(6,0);
-  // print the number of seconds since reset:
-  lcd.print(millis() / 1000);
-
-  lcd.setCursor(11, 1);
-  lcd.print(numClicks);
+  if(code.length() > 16){
+    lcd.setCursor(0,0);
+    code = code.substring(code.length()-1, code.length());
+    lcd.print("                                      ");
+  }
+  lcd.setCursor(0,0);
+  lcd.print(code);
 
   if(digitalRead(buttonPin) == HIGH){
     if(previousState == false){
-      numClicks += 1;
+      timeSinceLastChange = millis();
     }
     previousState = true;
   }
   else{
+    if(previousState == true){
+      if(millis()-timeSinceLastChange > 300){
+        code += "-";
+      }
+      else{
+        code += ".";
+      }
+      timeSinceLastChange = millis();
+    }
     previousState = false;
   }
 
