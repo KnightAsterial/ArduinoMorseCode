7176cfdcb070cb422ca59ceb9359d5d5221443f6
Miles <19bmiles@wsdweb.org>
2018-06-06 08:30:00 -0400

    This includes the first receiver test

---
+++ ReceiverTest/receiverTestCode/receiverTestCode.ino
@@ -0,0 +1,13 @@
+int sensor = -99; //change this
+int state = LOW;
+int val = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
