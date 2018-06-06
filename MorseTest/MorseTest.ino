06e502e5d128519804eec5bb588973adbe1d9984
Ryan Zhao <ryanzhao@live.com>
2018-06-06 07:38:05 -0400

    interprets Africa

--- MorseTest/MorseTest.ino
+++ MorseTest/MorseTest.ino
@@ -1,4 +1,5 @@
 #include <LiquidCrystal.h>
+#include "pitches.h"
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
@@ -60,6 +61,10 @@ void loop() {
     if(previousWordState == false){
       decoded += interpret(code);
       code = "";
+      lcd.setCursor(0,0);
+      lcd.print("                   ");
+      lcd.setCursor(0,1);
+      lcd.print("                   ");
       lcd.setCursor(0,1);
       lcd.print(decoded);
     }
@@ -69,17 +74,198 @@ void loop() {
     previousWordState = false;
   }
 
-  
+  if(code == "AFRICA"){
+    playSong();
+    code = "";
+    lcd.setCursor(0,1);
+    lcd.print("                   ");
+  }
 }
 
 String interpret(String code){
-  if(code == "."){
-    return "yep";
+  if(code == ".-"){
+    return "A";
+  }
+  else if (code == "..-."){
+    return "F";
+  }
+  else if (code == ".-."){
+    return "R";
+  }
+  else if (code == ".."){
+    return "I";
+  }
+  else if (code == "-.-."){
+    return "C";
   }
   else{
-    return "nope";
+    return "_";
   }
 }
 
 
+void playSong(){ 
+    // notes in the melody:
+    int melody[] = {
+     //DIFFERENT SONG BELOW
+     // NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+
+   
+     //BEGIN AFRICA
+     NOTE_G4, NOTE_G4, NOTE_G4, NOTE_G4,
+     NOTE_G4, NOTE_G4, NOTE_A4, NOTE_B4,
+     NOTE_C5, 0,
+     NOTE_E4, NOTE_E4, NOTE_F4, NOTE_G4,
+     NOTE_G4, NOTE_F4, NOTE_F4, NOTE_E4,
+     NOTE_D4, NOTE_D4, NOTE_D4, NOTE_C4,
+     NOTE_E4, NOTE_E4, NOTE_D4, NOTE_C4,
+     NOTE_D4, 0,
+     NOTE_D4,NOTE_C4, NOTE_F4, 
+     //START REPEAT
+     0, NOTE_G4, NOTE_G4, NOTE_G4,
+     NOTE_G4, NOTE_G4, NOTE_A4, NOTE_B4,
+     NOTE_C5, 0, NOTE_E4,
+     NOTE_E4, NOTE_E4, NOTE_F4, NOTE_G4,
+     NOTE_G4, NOTE_F4, NOTE_F4, NOTE_E4,
+     NOTE_D4, NOTE_D4, NOTE_D4, NOTE_C4,
+     NOTE_E4, NOTE_E4, NOTE_D4, NOTE_C4, 
+     NOTE_D4, 0, 
+     NOTE_D4, NOTE_C4, NOTE_F4,
+     0, NOTE_G4, NOTE_G4, NOTE_G4,
+     NOTE_G4, NOTE_G4, NOTE_A4, NOTE_B4,
+     NOTE_C5, NOTE_E4, NOTE_E4,
+     NOTE_E4, NOTE_E4, NOTE_E4, NOTE_F4, NOTE_G4,
+     NOTE_G4, NOTE_F4, NOTE_F4, NOTE_E4, NOTE_D4,
+     NOTE_D4, NOTE_C4, NOTE_D4, NOTE_C4,
+     NOTE_E4, NOTE_D4, NOTE_C4,
+     NOTE_D4, 0,
+     NOTE_D4, NOTE_C4, NOTE_F4,
+     0, NOTE_G4, NOTE_G4, NOTE_G4,
+     NOTE_G4, NOTE_G4, NOTE_A4, NOTE_B4,
+     NOTE_C5, 0,
+     NOTE_E5, NOTE_E5, NOTE_F5, NOTE_G5,
+     NOTE_G5, NOTE_F5, NOTE_F5,NOTE_E5, NOTE_F5,
+     NOTE_F5,
+     0,
+     0,
+     NOTE_AS5, NOTE_AS5, NOTE_AS5, NOTE_AS5, NOTE_AS5,
+     NOTE_AS5, NOTE_AS5, NOTE_AS5, NOTE_AS5, NOTE_AS5,
+     NOTE_AS5, NOTE_A5,
+     NOTE_A5,
+     0, NOTE_AS5, NOTE_AS5, NOTE_AS5, NOTE_AS5, NOTE_AS5,
+     NOTE_AS5, NOTE_AS5, NOTE_AS5, NOTE_AS5,
+     NOTE_AS5, NOTE_AS5, NOTE_AS5, NOTE_A5, 
+     NOTE_A5,
+     0, NOTE_AS5, NOTE_AS5, NOTE_AS5,
+     NOTE_AS5, NOTE_AS5, NOTE_AS5,
+     NOTE_AS5, NOTE_A5,
+     NOTE_A5,
+     0, NOTE_AS5, NOTE_AS5, NOTE_AS5, NOTE_AS5,
+     NOTE_AS5, NOTE_AS5, NOTE_AS5, NOTE_AS5,
+     NOTE_AS5, NOTE_AS5, NOTE_AS5, NOTE_A5,
+     NOTE_A5, NOTE_A5, NOTE_G5, NOTE_F5,
+     NOTE_A5, NOTE_A5, NOTE_AS5, NOTE_A5,
+     NOTE_G5, 0,
+     NOTE_D5, NOTE_D5, NOTE_D5,
+     NOTE_D5, NOTE_C5, NOTE_F5,
+     NOTE_F5,
+     0,
+     NOTE_D5, NOTE_D5, NOTE_D5,
+     NOTE_D5, NOTE_C5, NOTE_F5,
+     NOTE_F5, 0, //END REPEAT
+     NOTE_D4,
+     NOTE_D4, NOTE_C4, NOTE_F4,
+     NOTE_F4,
+     0
+     
+     
+     
+    };
+    
+    // note durations: 4 = quarter note, 8 = eighth note, etc.:
+    int noteDurations[] = {
+      2, 1, 1, 4,
+      2, 2, 2, 2,
+      4, 4,
+      2, 2, 2, 2,
+      2, 2, 2, 2,
+      2, 2, 2, 2,
+      4, 1, 1, 2,
+      4, 4,
+      2, 2, 4,
+      //START REPEAT
+      2, 2, 2, 2,
+      2, 2, 2, 2,
+      4, 2, 2,
+      2, 2, 2, 2,
+      2, 2, 2, 2,
+      2, 2, 2, 2,
+      4, 1, 1, 2,
+      4, 4,
+      2, 2, 4,
+      2, 2, 2, 2,
+      2, 2, 2, 2,
+      4, 2, 2,
+      1, 1, 2, 2, 2,
+      2, 2, 1, 1, 2,
+      2, 2, 2, 2,
+      4, 2, 2,
+      4, 4,
+      2, 2, 4,
+      2, 2, 2, 2,
+      2, 2, 2, 2,
+      4, 4,
+      2, 2, 2, 2,
+      2, 2, 1, 1, 2,
+      8,
+      8,
+      8,
+      2, 1, 1, 2, 2,
+      2, 2, 1, 1, 2,
+      6, 2,
+      8,
+      2, 2, 1, 1, 1, 1,
+      2, 2, 2, 2,
+      3, 1, 2, 2,
+      8,
+      2, 2, 2, 2,
+      4, 2, 2,
+      6, 2,
+      8,
+      2, 1, 1, 2, 2,
+      2, 2, 2, 2,
+      2, 2, 2, 2,
+      4, 1, 1, 2,
+      4, 1, 1, 2,
+      6, 2,
+      3, 1, 4,
+      2, 2, 4,
+      8,
+      8,
+      3, 1, 4,
+      2, 2, 4,
+      8,
+      8,
+      8,
+      2, 2, 4,
+      8,
+      8
+      
+      
+    };
+    
+    for (int thisNote = 0; thisNote < 201; thisNote++) {
+      // to calculate the note duration, take one second divided by the note type.
+      //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+      int noteDuration = 120* noteDurations[thisNote];
+      tone(8, melody[thisNote], noteDuration);
+  
+      // to distinguish the notes, set a minimum time between them.
+      // the note's duration + 30% seems to work well:
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+      // stop the tone playing:
+      noTone(8);
+    }
+}
 
