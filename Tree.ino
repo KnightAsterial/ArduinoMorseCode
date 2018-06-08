5ee048806645072ea84246223ad477146ce5df76
Alztec24 <39742767+Alztec24@users.noreply.github.com>
2018-06-07 20:43:23 -0400

    Add files via upload

---
+++ Tree.ino
@@ -0,0 +1,358 @@
+#include <LiquidCrystal.h>
+#include "pitches.h"
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+
+
+const int buttonPin = 7;
+bool previousState = false;
+
+const int wordButtonPin = 6;
+bool previousWordState = false;
+
+long timeSinceLastChange = 0;
+String code = "";
+
+String decoded = "";
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+
+  timeSinceLastChange = millis();
+
+  pinMode(buttonPin, INPUT);
+  pinMode(wordButtonPin, INPUT);
+}
+
+void loop() {
+  // set the cursor to column 0, line 1
+  if(code.length() > 16){
+    lcd.setCursor(0,0);
+    code = code.substring(code.length()-1, code.length());
+    lcd.print("                                      ");
+  }
+  lcd.setCursor(0,0);
+  lcd.print(code);
+
+  if(digitalRead(buttonPin) == HIGH){
+    if(previousState == false){
+      timeSinceLastChange = millis();
+    }
+    previousState = true;
+  }
+  else{
+    if(previousState == true){
+      if(millis()-timeSinceLastChange > 300){
+        code += "-";
+      }
+      else{
+        code += ".";
+      }
+      timeSinceLastChange = millis();
+    }
+    previousState = false;
+  }
+
+  //----
+  if(digitalRead(wordButtonPin) == HIGH){
+    if(previousWordState == false){
+      decoded += interpret(code);     //The call for interpretation
+      code = "";
+      lcd.setCursor(0,0);
+      lcd.print("                   ");
+      lcd.setCursor(0,1);
+      lcd.print("                   ");
+      lcd.setCursor(0,1);
+      lcd.print(decoded);
+    }
+    previousWordState = true;
+  }
+  else{
+    previousWordState = false;
+  }
+
+  if(code == "AFRICA"){
+    playSong();
+    code = "";
+    lcd.setCursor(0,1);
+    lcd.print("                   ");
+  }
+}
+
+String interpret(String code){
+  int length = code.length;
+  if(code.charAt(0) == '.' && length > 1){//.cont
+    if(code.charAt(1) == '.' && length > 2){//..cont
+      if(code.charAt(2) == '.' && length > 3){//...cont
+        if(code.charAt(3) == '.'){
+          return "H";//....
+        }
+        return "V";//..._
+      }
+      else if(code.charAt(2) == '_' && length > 3){//.._cont
+        if(code.charAt(3) == '.'){
+          return "F";//.._.
+        }
+      }
+      else{
+        if(code.charAt(2) == '.'){
+          return "S";//...
+        }
+        return "U";//.._
+      }
+    }
+    else if(code.charAt(1) == '_' && length > 2){//._cont
+      if(code.charAt(2) == '.' && length > 3){//._.cont
+        if(code.charAt(3) == '.'){
+          return "L";//._..
+        }
+      }
+      else if(code.charAt(2) == '_' && length > 3){//.__cont
+        if(code.charAt(3) == '.'){
+          return "P";//.__.
+        }
+        return "J";//.___
+      }
+      else{
+        if(code.charAt(2) == '.'){
+          return "R";//...
+        }
+        return "W";//.._
+      }
+    }
+    else{
+      if(code.charAt(1) == '.'){
+        return "I";//..
+      }
+      return "A";//._
+    }
+  }
+  else if(code.charAt(0) == '_' && length > 1){//_cont
+    if(code.charAt(1) == '.' && length > 2){//_.cont
+      if(code.charAt(2) == '.' && length > 3){//_..cont
+        if(code.charAt(3) == '.'){
+          return "B";//_...
+        }
+        return "X";//_.._
+      }
+      else if(code.charAt(2) == '_' && length > 3){//_._cont
+        if(code.charAt(3) == '.'){
+          return "C";//_._.
+        }
+        return "Y";//_.__
+      }
+      else{
+        if(code.charAt(2) == '.'){
+          return "D";//_..
+        }
+        return "K";//_._
+      }
+    }
+    else if(code.charAt(1) == '_' && length > 2){//__cont
+      if(code.charAt(2) == '.' && length > 3){//__.cont
+        if(code.charAt(3) == '.'){
+          return "Z";//__..
+        }
+        return "Q";//__._
+      }
+      else{
+        if(code.charAt(2) == '.'){
+          return "G";//__.
+        }
+        return "O";//___
+      }
+    }
+    else{
+      if(code.charAt(1) == '.'){
+        return "N";//_.
+      }
+      return "M";//__
+    }
+  }
+  else{
+    if(code.charAt(0) == '.'){
+      return "E";//.
+    }
+    else if(code.charAt(0) == '_'){
+      return "T";//_
+    }
+    else{
+      return "_";
+    }
+  }
+}
+
+void decoding(){
+  //Everytime that
+  
+}
+
+void playSong(){ 
+    // notes in the melody:
+    int melody[] = {
+     //DIFFERENT SONG BELOW
+     // NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+
+   
+     //BEGIN AFRICA
+     NOTE_G4, NOTE_G4, NOTE_G4, NOTE_G4,
+     NOTE_G4, NOTE_G4, NOTE_A4, NOTE_B4,
+     NOTE_C5, 0,
+     NOTE_E4, NOTE_E4, NOTE_F4, NOTE_G4,
+     NOTE_G4, NOTE_F4, NOTE_F4, NOTE_E4,
+     NOTE_D4, NOTE_D4, NOTE_D4, NOTE_C4,
+     NOTE_E4, NOTE_E4, NOTE_D4, NOTE_C4,
+     NOTE_D4, 0,
+     NOTE_D4,NOTE_C4, NOTE_F4, 
+     //START REPEAT
+     0, NOTE_G4, NOTE_G4, NOTE_G4,
+     NOTE_G4, NOTE_G4, NOTE_A4, NOTE_B4,
+     NOTE_C5, 0, NOTE_E4,
+     NOTE_E4, NOTE_E4, NOTE_F4, NOTE_G4,
+     NOTE_G4, NOTE_F4, NOTE_F4, NOTE_E4,
+     NOTE_D4, NOTE_D4, NOTE_D4, NOTE_C4,
+     NOTE_E4, NOTE_E4, NOTE_D4, NOTE_C4, 
+     NOTE_D4, 0, 
+     NOTE_D4, NOTE_C4, NOTE_F4,
+     0, NOTE_G4, NOTE_G4, NOTE_G4,
+     NOTE_G4, NOTE_G4, NOTE_A4, NOTE_B4,
+     NOTE_C5, NOTE_E4, NOTE_E4,
+     NOTE_E4, NOTE_E4, NOTE_E4, NOTE_F4, NOTE_G4,
+     NOTE_G4, NOTE_F4, NOTE_F4, NOTE_E4, NOTE_D4,
+     NOTE_D4, NOTE_C4, NOTE_D4, NOTE_C4,
+     NOTE_E4, NOTE_D4, NOTE_C4,
+     NOTE_D4, 0,
+     NOTE_D4, NOTE_C4, NOTE_F4,
+     0, NOTE_G4, NOTE_G4, NOTE_G4,
+     NOTE_G4, NOTE_G4, NOTE_A4, NOTE_B4,
+     NOTE_C5, 0,
+     NOTE_E5, NOTE_E5, NOTE_F5, NOTE_G5,
+     NOTE_G5, NOTE_F5, NOTE_F5,NOTE_E5, NOTE_F5,
+     NOTE_F5,
+     0,
+     0,
+     NOTE_AS5, NOTE_AS5, NOTE_AS5, NOTE_AS5, NOTE_AS5,
+     NOTE_AS5, NOTE_AS5, NOTE_AS5, NOTE_AS5, NOTE_AS5,
+     NOTE_AS5, NOTE_A5,
+     NOTE_A5,
+     0, NOTE_AS5, NOTE_AS5, NOTE_AS5, NOTE_AS5, NOTE_AS5,
+     NOTE_AS5, NOTE_AS5, NOTE_AS5, NOTE_AS5,
+     NOTE_AS5, NOTE_AS5, NOTE_AS5, NOTE_A5, 
+     NOTE_A5,
+     0, NOTE_AS5, NOTE_AS5, NOTE_AS5,
+     NOTE_AS5, NOTE_AS5, NOTE_AS5,
+     NOTE_AS5, NOTE_A5,
+     NOTE_A5,
+     0, NOTE_AS5, NOTE_AS5, NOTE_AS5, NOTE_AS5,
+     NOTE_AS5, NOTE_AS5, NOTE_AS5, NOTE_AS5,
+     NOTE_AS5, NOTE_AS5, NOTE_AS5, NOTE_A5,
+     NOTE_A5, NOTE_A5, NOTE_G5, NOTE_F5,
+     NOTE_A5, NOTE_A5, NOTE_AS5, NOTE_A5,
+     NOTE_G5, 0,
+     NOTE_D5, NOTE_D5, NOTE_D5,
+     NOTE_D5, NOTE_C5, NOTE_F5,
+     NOTE_F5,
+     0,
+     NOTE_D5, NOTE_D5, NOTE_D5,
+     NOTE_D5, NOTE_C5, NOTE_F5,
+     NOTE_F5, 0, //END REPEAT
+     NOTE_D4,
+     NOTE_D4, NOTE_C4, NOTE_F4,
+     NOTE_F4,
+     0
+     
+     
+     
+    };
+    
+    // note durations: 4 = quarter note, 8 = eighth note, etc.:
+    int noteDurations[] = {
+      2, 1, 1, 4,
+      2, 2, 2, 2,
+      4, 4,
+      2, 2, 2, 2,
+      2, 2, 2, 2,
+      2, 2, 2, 2,
+      4, 1, 1, 2,
+      4, 4,
+      2, 2, 4,
+      //START REPEAT
+      2, 2, 2, 2,
+      2, 2, 2, 2,
+      4, 2, 2,
+      2, 2, 2, 2,
+      2, 2, 2, 2,
+      2, 2, 2, 2,
+      4, 1, 1, 2,
+      4, 4,
+      2, 2, 4,
+      2, 2, 2, 2,
+      2, 2, 2, 2,
+      4, 2, 2,
+      1, 1, 2, 2, 2,
+      2, 2, 1, 1, 2,
+      2, 2, 2, 2,
+      4, 2, 2,
+      4, 4,
+      2, 2, 4,
+      2, 2, 2, 2,
+      2, 2, 2, 2,
+      4, 4,
+      2, 2, 2, 2,
+      2, 2, 1, 1, 2,
+      8,
+      8,
+      8,
+      2, 1, 1, 2, 2,
+      2, 2, 1, 1, 2,
+      6, 2,
+      8,
+      2, 2, 1, 1, 1, 1,
+      2, 2, 2, 2,
+      3, 1, 2, 2,
+      8,
+      2, 2, 2, 2,
+      4, 2, 2,
+      6, 2,
+      8,
+      2, 1, 1, 2, 2,
+      2, 2, 2, 2,
+      2, 2, 2, 2,
+      4, 1, 1, 2,
+      4, 1, 1, 2,
+      6, 2,
+      3, 1, 4,
+      2, 2, 4,
+      8,
+      8,
+      3, 1, 4,
+      2, 2, 4,
+      8,
+      8,
+      8,
+      2, 2, 4,
+      8,
+      8
+      
+      
+    };
+    
+    for (int thisNote = 0; thisNote < 201; thisNote++) {
+      // to calculate the note duration, take one second divided by the note type.
+      //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+      int noteDuration = 120* noteDurations[thisNote];
+      tone(8, melody[thisNote], noteDuration);
+  
+      // to distinguish the notes, set a minimum time between them.
+      // the note's duration + 30% seems to work well:
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+      // stop the tone playing:
+      noTone(8);
+    }
+}
